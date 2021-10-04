e27df262b3ae827536244248bcd91b9cc22ccb04
Jordan Lesaca <jordan.m.lesaca@gmail.com>
2021-10-04 13:06:23 -0700

    got rid of favorite button on RaveCard as it had no real function other than filling in a heart

--- src/RaveCard.js
+++ src/RaveCard.js
@@ -1,7 +1,6 @@
-import React, { useState } from 'react'
+import React from 'react'
 
 function RaveCard({ rave, deleteRave, updateRave}){
-    const [favorite, setFavorite] = useState(false)
     const { id, 
         artist, 
         image, 
@@ -9,16 +8,13 @@ function RaveCard({ rave, deleteRave, updateRave}){
         city, 
         date, 
         likes } = rave
+        const defaultPic = "https://cdn.shopify.com/s/files/1/0385/6229/files/d14211d213a7f0fbad2d13d08d3a4580c63e1dcf_large.jpg?v=1482619950https://cdn.shopify.com/s/files/1/0385/6229/files/d14211d213a7f0fbad2d13d08d3a4580c63e1dcf_large.jpg?v=1482619950"
 
     function handleDeleteClick(){
         fetch(`http://localhost:3000/raves/${id}`, {
-      method: "DELETE"
-    })
-    deleteRave(id)
-    }
-
-    function handleFavorite(){
-        setFavorite(favorite => !favorite)
+        method: "DELETE"
+            })
+        deleteRave(id)
     }
 
     function handleLikes(){
@@ -30,7 +26,8 @@ function RaveCard({ rave, deleteRave, updateRave}){
             method: "PATCH",
             headers: {"content-type" : "application/json"},
             body: JSON.stringify(updatedRave)
-            }).then(res => res.json())
+                })
+            .then(res => res.json())
             .then(updateRave)
         }
 
@@ -43,34 +40,26 @@ function RaveCard({ rave, deleteRave, updateRave}){
             method: "PATCH",
             headers: {"content-type" : "application/json"},
             body: JSON.stringify(updatedRave)
-            }).then(res => res.json())
+                })
+            .then(res => res.json())
             .then(updateRave)
         }
-
-
-    const defaultPic = "https://cdn.shopify.com/s/files/1/0385/6229/files/d14211d213a7f0fbad2d13d08d3a4580c63e1dcf_large.jpg?v=1482619950https://cdn.shopify.com/s/files/1/0385/6229/files/d14211d213a7f0fbad2d13d08d3a4580c63e1dcf_large.jpg?v=1482619950"
     
-
     return(
-        <div className="" >
-        
+        <div className="" >   
             <h3>{artist}</h3>
+                <p>Likes: {likes}</p>          
+            <h4>Date: {!date ? "TBA" : date} </h4>
+                <p>Venue, City: {!venue ? "TBA" : venue}, {!city ? "TBA" : city} </p>
             <button className="delete-button" onClick={handleDeleteClick} >Delete</button>
             <button className="like-button" onClick={handleLikes} >Like</button>
             <button className="dislike-button" onClick={handleDislikes} >Dislike</button>
-            {favorite ? (
-                <button onClick={handleFavorite} className="favorite">♥</button>
-            ) : ( 
-                <button onClick={handleFavorite} className="notFavorite">♡</button>
-            )
-            }
-            <p>Likes: {likes}</p>
-            <h4>Date: {!date ? "TBA" : date} </h4>
-            <p>Venue, City: {!venue ? "TBA" : venue}, {!city ? "TBA" : city} </p>
-            <img src={!image ? defaultPic : image} alt={artist} style={{
-                height: 200,
-                width: 200
-              }} /> 
+            <p>Album Art:</p>
+                <img src={!image ? defaultPic : image} alt={artist} style={{
+                    height: 200,
+                    width: 200
+                }}
+                /> 
         </div>
     )
 }
