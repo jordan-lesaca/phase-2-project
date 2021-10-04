71eeccc4608bff34d52418b9da349b2b75aef3fb
Jordan Lesaca <jordan.m.lesaca@gmail.com>
2021-10-04 12:57:36 -0700

    got rid of white space on Home.js

--- src/Home.js
+++ src/Home.js
@@ -2,19 +2,17 @@ import React from 'react'
 import HomeCard from './HomeCard'
 
 function Home({raves}){
-
-
     return (
         <div id="rave-collection">
         <h2>List of Artists with upcoming raves:</h2>
           {raves.map(rave => {
             return (
-              <HomeCard rave={rave} key={rave.id} />  
-               )
+              <HomeCard rave={rave} key={rave.id} /> 
+              )
             })
           }
         </div>
       );
     }
-
+    
 export default Home
