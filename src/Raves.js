c62e047a373643d7c9b45f7cc2778964a42143f4
Jordan Lesaca <jordan.m.lesaca@gmail.com>
2021-10-04 13:11:28 -0700

    added search bar

--- src/Raves.js
+++ src/Raves.js
@@ -1,21 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
 import RaveCard from './RaveCard'
 
 function Raves({raves, deleteRave, updateRave }) {
-
+  const [searchTerm, setSearchTerm] = useState("")
 
     return (
-
       <div id="rave-collection">
-        {raves.map(rave => {
+      <h2> Search </h2>
+      <input 
+      type="text"
+      placeholder="Search artist"
+      onChange={(e) => {
+          setSearchTerm(e.target.value)}}
+      />
+        {raves.filter((rave) => {
+          if (searchTerm === "") {
+            return rave 
+        } else if (rave.artist.toLowerCase()
+        .includes(searchTerm.toLowerCase())) 
+        {
+            return rave
+        }
+      return false
+        })
+        .map(rave => {
           return (
             <RaveCard 
             rave={rave} 
             key={rave.id} 
             deleteRave={deleteRave} 
             updateRave={updateRave} />
-                            )
-          })
+                  )
+        })
         }
       </div>
     );
